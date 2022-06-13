89128e0aca28b50da6954908ea4e8115444c66b4
magerngulik <magerngulik@gmail.com>
2022-06-13 20:16:48 +0700

    create data ke mysql rest api

--- crud/data.php
+++ crud/data.php
@@ -2,7 +2,7 @@
 
     require_once('helper.php');
 
-    $query = "SELECT * FROM notes";
+    $query = "SELECT * FROM notes ORDER BY id DESC";
     $sql = mysqli_query($db_connect, $query);
 
     if ($sql) 
@@ -13,10 +13,8 @@
             array_push($result, array(
                 'id' => $row['id'],
                 'note' => $row['note'],
-         
             ));
         }
-
         echo json_encode(array('result'=>$result));
         
     }
