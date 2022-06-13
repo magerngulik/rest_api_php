6d43f8345a7762eb51b4dd9563ed2524be6dc7da
magerngulik <magerngulik@gmail.com>
2022-06-13 19:00:28 +0700

    Menambahkan helper connection ke mysql database

---
+++ crud/helper.php
@@ -0,0 +1,8 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('DB', 'lazdays_crud');
+    $db_connect = mysqli_connect(HOST, USER, PASSWORD, DB) or die('Error connecting to MySQL server.');
+
+?>
