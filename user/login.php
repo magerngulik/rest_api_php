37456edf0f58d7abb33a1e8e44f74138ddb6a91d
magerngulik <magerngulik@gmail.com>
2022-06-13 22:26:51 +0700

    melakukan update pada last_login di field user pada mysql

--- user/login.php
+++ user/login.php
@@ -14,7 +14,27 @@
         respone(400,array('message'=>'User belum terdaftar!'));
       }else
         {
-            respone(200,array('message'=>'User terdaftar!'));
+            //untuk melakukan update data pada login terakhir user
+            $date = date('Y-m-d');
+            $update ="UPDATE user set last_login='$date' where email='$email'";
+            mysqli_query($db_connect, $update);
+
+            //untuk mengambil data user yang sudah login
+            $user =mysqli_query($db_connect, $query);
+            $result = array();
+            while($row = mysqli_fetch_array($user))
+            {
+                array_push($result, array(
+                    'id' => $row['id'],
+                    'name' => $row['name'],
+                    'email' => $row['email'],
+                    'last_login' => $row['last_login'],
+                ));
+            }
+            respone(200,
+            array('message'=>'Data berhasil di masukan!',
+            'result'=>$result
+        ));
         } 
     }else{
         respone(400,array('message'=>'error!'));
