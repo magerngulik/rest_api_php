ae4dcdf78949bea0149e55cc412db2efa54dc5a7
magerngulik <magerngulik@gmail.com>
2022-06-13 21:18:22 +0700

    menambahkan logic ketika email sudah terdaftar

--- user/register.php
+++ user/register.php
@@ -6,14 +6,31 @@
     $email =$_POST['email'];
     $password =md5($_POST['password']);
 
-    $query = "INSERT INTO user(name,email,password) VALUES('$name','$email','$password')";
-    $sql = mysqli_query($db_connect, $query);
+    $query = "SELECT email FROM user where email='$email'";
+    $check = mysqli_query($db_connect, $query);
 
-    if ($sql) 
-    {
-        respone(200,array('message'=>'berhasil register!')); 
-    }else {
-        respone(400,array('message'=>'gagal register!'));
+    if ($check) {
+        //cek data nya ada atau tidak
+        if (empty(mysqli_fetch_array($check))) {
+            $query = "INSERT INTO user(name,email,password) VALUES('$name','$email','$password')";
+            $sql = mysqli_query($db_connect, $query);
+        
+            if ($sql) 
+            {
+                respone(200,array('message'=>'berhasil register!')); 
+            }else {
+                respone(400,array('message'=>'gagal register!'));
+            }
+        }else{
+            respone(400,array('message'=>'email sudah terdaftar!'));
+        }
+
+
+
+
     }
+
     mysqli_close($db_connect);
+
+   
 ?>
