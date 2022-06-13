0401b1ce434c37b16957ee9ea119f3a8981886bf
magerngulik <magerngulik@gmail.com>
2022-06-13 19:24:44 +0700

    Menambahkan header agar file yang di hasilkan tetap dalam bentuk json

--- crud/helper.php
+++ crud/helper.php
@@ -5,4 +5,5 @@
     define('DB', 'lazdays_crud');
     $db_connect = mysqli_connect(HOST, USER, PASSWORD, DB) or die('Error connecting to MySQL server.');
 
+    header("Content-Type: application/json");
 ?>
