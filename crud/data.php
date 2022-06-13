317f85312ee912005476a643c9a27091c4cc9051
magerngulik <magerngulik@gmail.com>
2022-06-13 19:22:46 +0700

    Menambahkan rest api untuk get data dari database mysql

---
+++ crud/data.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once('helper.php');
+
+    $query = "SELECT * FROM notes";
+    $sql = mysqli_query($db_connect, $query);
+
+    if ($sql) 
+    {
+        $result = array();
+        while ($row = mysqli_fetch_array($sql))
+        {
+            array_push($result, array(
+                'id' => $row['id'],
+                'note' => $row['note'],
+         
+            ));
+        }
+
+        echo json_encode($result);
+        
+    }
+
+
+?>
